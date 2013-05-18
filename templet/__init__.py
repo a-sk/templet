eabe4c42effeaa9c43cba1ad5197e61d9edc0182
a-sk <a.skurihin@gmail.com>
2013-05-19 01:01:55 +0400

    First working draft

--- templet/__init__.py
+++ templet/__init__.py
@@ -1,9 +1,18 @@
 import tempita
 import shutil
 
-def rename(src, template_data):
-    if os.path.isfile(src):
-        shutil.move(src,)
+
+__all__ = ['handle_project']
+
+
+def copy(src, dst):
+    shutil.copy(src, dst)
+    return dst
+
+
+def move(src, dst):
+    shutil.move(src, dst)
+    return dst
 
 
 def expand_vars_in_file(filepath, project_root, template_data):
@@ -13,27 +22,26 @@ def expand_vars_in_file(filepath, project_root, template_data):
     with open(filepath, 'w', encoding='utf8') as f:
         f.write(file_contents)
 
+
 def expand_vars_in_file_name(filepath, template_data):
     tmpl = tempita.Template(os.path.basename(filepath))
     return tmpl.substitute(template_data)
 
 
-def handle_project(project_path, project_root, template_data):
+def handle_project(src, dst, template_data):
+    copy(src, dst)
     dirs_to_rename = []
-    for root, dirs, files in os.walk(project_root):
+    for root, dirs, files in os.walk(dst):
         for f in files:
             filepath = os.path.join(root, f)
 
             if os.path.isfile(filepath):
                 expand_vars_in_file(filepath)
 
-            # expand files
-            new_filepath = rename(filepath, template_data)
-            # expand dirs
-            if os.path.isdir(filepath) and filepath.startswith('{{') and filepath.endswith('}}'):
-                rename(filepath, template_data)
-
-
-def copy_template(src, dst):
-    return dst
+            if filepath.startswith('{{') and filepath.endswith('}}'):
+                # expand vars in file name
+                new_filepath = move(filepath, template_data)
+                # expand vars in dir name, it should be done in the last turn
+                if os.path.isdir(filepath):
+                    move(filepath, template_data)
 
