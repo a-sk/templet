279ad22258edafa9ce5ccd825631d22510f50729
a-sk <a.skurihin@gmail.com>
2013-05-19 02:46:17 +0400

    Version bump

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requires = [
 
 setup(
     name='templet',
-    version='0.0.3',
+    version='0.0.4',
     description='Simple scaffolding tool',
     long_description=open('README.md').read(),
     author='Alexandr Skurikhin',
