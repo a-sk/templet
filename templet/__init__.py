b1a9e5c251fb2c0e9f9b8209fe61982bed09cdd2
a-sk <a.skurihin@gmail.com>
2013-05-19 02:52:19 +0400

    Add maybe_rename function, remove duplicate code

--- templet/__init__.py
+++ templet/__init__.py
@@ -13,11 +13,17 @@ def copy(src, dst):
         shutil.copytree(src, dst)
     return dst
 
-
 def move(src, dst):
     shutil.move(src, dst)
     return dst
 
+def maybe_rename(src, template_data):
+    """Rename file or directory if it's name contains expandable variable"""
+    if os.path.basename(src).startswith('{{') \
+        and os.path.basename(src).endswith('}}'):
+
+        new_path = expand_vars_in_file_name(src, template_data)
+        move(src, new_path)
 
 def expand_vars_in_file(filepath, project_root, template_data):
     with open(filepath) as fp:
@@ -26,11 +32,9 @@ def expand_vars_in_file(filepath, project_root, template_data):
     with open(filepath, 'w') as f:
         f.write(file_contents)
 
-
 def expand_vars_in_file_name(filepath, template_data):
     tmpl = tempita.Template(filepath)
     return tmpl.substitute(template_data)
-
 
 def handle_project(src, dst, template_data):
     copy(src, dst)
@@ -38,20 +42,11 @@ def handle_project(src, dst, template_data):
     for root, dirs, files in os.walk(dst):
         for d in dirs:
             dirpath = os.path.join(root, d)
-            if os.path.basename(dirpath).startswith('{{') \
-                and os.path.basename(dirpath).endswith('}}'):
-
-                new_dirpath = expand_vars_in_file_name(dirpath, template_data)
-                move(dirpath, new_dirpath)
+            maybe_rename(dirpath, template_data)
 
         for f in files:
             filepath = os.path.join(root, f)
             if os.path.isfile(filepath):
                 expand_vars_in_file(filepath, dst, template_data)
-
-            if os.path.basename(filepath).startswith('{{') \
-                and os.path.basename(filepath).endswith('}}'):
-
-                new_filepath = expand_vars_in_file_name(filepath, template_data)
-                move(filepath, new_filepath)
+            maybe_rename(filepath, template_data)
 
